219fd980d823a18707d68ebf80eb166eb205319b
Benjamin Kovach <bkovach13@gmail.com>
2014-04-20 17:47:43 -0400

    HashSet/Map -> []

--- src/Graphene/Graph.hs
+++ src/Graphene/Graph.hs
@@ -5,56 +5,60 @@ module Graphene.Graph where
 import qualified Data.HashSet      as HS
 import qualified Data.HashMap.Lazy as HM
 import Data.Hashable
-import Data.List(foldl')
+import Data.List
+import Data.Function
 import Control.Lens
 
 data Graph e v = Graph
-  { _vertices :: HS.HashSet v
-  , _edges    :: HM.HashMap e (v, v)
+  { _vertices :: [v]
+  , _edges    :: [(e, (v, v))]
   } deriving (Show, Eq)
 
 makeLenses ''Graph
 
 emptyGraph :: Graph e v
-emptyGraph = Graph HS.empty HM.empty
+emptyGraph = Graph [] []
 
-insertVertex :: (Eq v, Hashable v) => v -> Graph e v -> Graph e v
-insertVertex !v (Graph vs es) = Graph (HS.insert v vs) es
+insertVertex :: v -> Graph e v -> Graph e v
+insertVertex !v (Graph vs es) = Graph (v:vs) es
 
-removeVertex :: (Eq v, Hashable v) => v -> Graph e v -> Graph e v
-removeVertex v g =  vertices %~ (HS.delete v) $ edges %~ (HM.filterWithKey (\_ (v1, v2) -> not $ any (==v) [v1, v2])) $ g
+removeVertex :: Eq v => v -> Graph e v -> Graph e v
+removeVertex v g = vertices %~ (delete v) 
+  $ edges %~ (filter (\(_, (v1, v2)) -> not $ any (==v) [v1, v2])) $ g
 
-removeEdge :: (Eq e, Hashable e) => e -> Graph e v -> Graph e v
-removeEdge e = edges %~ (HM.delete e)
+removeEdge :: Eq e => e -> Graph e v -> Graph e v
+removeEdge e = edges %~ (deleteBy ((==) `on` fst) (e, undefined))
 
-insertEdge :: (Eq v, Eq e, Hashable v, Hashable e) => 
-  e -> (v, v) -> Graph e v -> Graph e v 
+insertEdge :: e -> (v, v) -> Graph e v -> Graph e v 
 insertEdge !e !(v, v') (Graph vs es) = 
-  foldr insertVertex (Graph vs (HM.insert e (v, v') es)) [v, v']
+  foldr insertVertex (Graph vs ((e, (v, v')):es)) [v, v']
   
-insertVertices :: (Eq b, Hashable b) => [b] -> Graph e b -> Graph e b
+insertVertices :: (Eq b) => [b] -> Graph e b -> Graph e b
 insertVertices vs g = foldl' (flip insertVertex) g vs
 
-insertEdges :: (Eq v, Eq e, Hashable v, Hashable e) =>
-     [(e, v, v)] -> Graph e v -> Graph e v
+insertEdges :: [(e, v, v)] -> Graph e v -> Graph e v
 insertEdges es g = foldl' (\g (e, v1, v2) -> insertEdge e (v1, v2) g) g es 
 
 connections :: (Eq v) => v -> Graph e v -> [(e, (v, v))]
 connections !v (Graph _ es) = 
-  HM.toList $ HM.filter (\(v1, v2) -> any (==v) [v1, v2]) es
+  filter (\(_, (v1, v2)) -> any (==v) [v1, v2]) es
 
 neighbors :: Eq v => v -> Graph e v -> [v]
 neighbors !v (Graph _ es) = 
-  HM.foldlWithKey'
-  (\acc e (v1, v2) -> if v == v2 then (v1:acc) else if v == v1 then (v2:acc) else acc)
+  foldl'
+  (\acc (e, (v1, v2)) -> if v == v2 then (v1:acc) else if v == v1 then (v2:acc) else acc)
   []
   es
 
-adjacentVertices :: (Eq e, Hashable e) => e -> Graph e v -> Maybe (v, v)
-adjacentVertices !e (Graph _ es) = HM.lookup e es
+adjacentVertices :: (Eq e) => e -> Graph e v -> Maybe (v, v)
+adjacentVertices !e (Graph _ es) = lookup e es
 
-fromLists :: (Hashable e, Hashable v, Eq e, Eq v) => [v] -> [(e, v, v)] -> Graph e v
+fromLists :: (Eq v) => [v] -> [(e, v, v)] -> Graph e v
 fromLists vs es = insertEdges es $ insertVertices vs emptyGraph
 
 degree :: Eq v => v -> Graph e v -> Int
 degree v = length . connections v
+
+-- map over vertices
+instance Functor (Graph e) where
+  fmap f (Graph vs es) = Graph (map f vs) (map (\(e, (v1, v2)) -> (e, (f v1, f v2))) es)
