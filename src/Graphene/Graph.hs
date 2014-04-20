0e584399426c83d138d3c37086993b24e18bb8a1
Benjamin Kovach <bkovach13@gmail.com>
2014-04-20 16:33:50 -0400

    add insertEdges and insertVertices

--- src/Graphene/Graph.hs
+++ src/Graphene/Graph.hs
@@ -1,10 +1,11 @@
 
-{-# LANGUAGE BangPatterns, TemplateHaskell #-}
+{-# LANGUAGE BangPatterns, TemplateHaskell, NoMonomorphismRestriction #-}
 module Graphene.Graph where
 
 import qualified Data.HashSet      as HS
 import qualified Data.HashMap.Lazy as HM
 import Data.Hashable
+import Data.List(foldl')
 import Control.Lens
 
 data Graph e v = Graph
@@ -24,13 +25,20 @@ insertEdge :: (Eq v, Eq e, Hashable v, Hashable e) =>
   e -> (v, v) -> Graph e v -> Graph e v 
 insertEdge !e !(v, v') (Graph vs es) = 
   foldr insertVertex (Graph vs (HM.insert e (v, v') es)) [v, v']
+  
+insertVertices :: (Eq b, Hashable b) => [b] -> Graph e b -> Graph e b
+insertVertices vs g = foldl' (flip insertVertex) g vs
+
+insertEdges :: (Eq v, Eq e, Hashable v, Hashable e) =>
+     [(e, v, v)] -> Graph e v -> Graph e v
+insertEdges es g = foldl' (\g (e, v1, v2) -> insertEdge e (v1, v2) g) g es 
 
 connections :: (Eq v) => v -> Graph e v -> [(e, (v, v))]
 connections !v (Graph _ es) = 
   HM.toList $ HM.filter (\(v1, v2) -> any (==v) [v1, v2]) es
 
 neighbors :: Eq v => v -> Graph e v -> [v]
-neighbors v (Graph _ es) = 
+neighbors !v (Graph _ es) = 
   HM.foldlWithKey'
   (\acc e (v1, v2) -> if v == v2 then (v1:acc) else if v == v1 then (v2:acc) else acc)
   []
@@ -40,5 +48,4 @@ adjacentVertices :: (Eq e, Hashable e) => e -> Graph e v -> Maybe (v, v)
 adjacentVertices !e (Graph _ es) = HM.lookup e es
 
 fromLists :: (Hashable e, Hashable v, Eq e, Eq v) => [v] -> [(e, v, v)] -> Graph e v
-fromLists vs es = foldr insertVertex edgeGraph vs
-  where edgeGraph = foldr (\(e, v1, v2) -> insertEdge e (v1, v2)) emptyGraph es
+fromLists vs es = insertEdges es $ insertVertices vs emptyGraph
