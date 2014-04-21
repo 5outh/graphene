2f598e79a9ff7da810cd0d96ba720d0e7d09f7e4
Benjamin Kovach <bkovach13@gmail.com>
2014-04-20 20:36:41 -0400

    Add instances

--- src/Graphene/Graph.hs
+++ src/Graphene/Graph.hs
@@ -2,12 +2,15 @@
 {-# LANGUAGE BangPatterns, TemplateHaskell, NoMonomorphismRestriction #-}
 module Graphene.Graph where
 
-import qualified Data.HashSet      as HS
-import qualified Data.HashMap.Lazy as HM
 import Data.Hashable
 import Data.List
 import Data.Function
 import Control.Lens
+import Data.Bifunctor
+import qualified Data.Foldable as F
+import Data.Bifoldable
+import Data.Traversable
+import Data.Monoid
 
 data Graph e v = Graph
   { _vertices :: [v]
@@ -62,3 +65,20 @@ degree v = length . connections v
 -- map over vertices
 instance Functor (Graph e) where
   fmap f (Graph vs es) = Graph (map f vs) (map (\(e, (v1, v2)) -> (e, (f v1, f v2))) es)
+
+-- map over both vertices and edges
+instance Bifunctor Graph where
+  bimap f g (Graph vs es) = Graph (map g vs) (map (\(e, (v1, v2)) -> (f e, (g v1, g v2))) es)
+
+-- fold over vertices
+instance F.Foldable (Graph e) where
+  foldMap f = F.foldMap f . view vertices
+
+-- fold over both vertices and edges
+instance Bifoldable Graph where
+  bifoldMap f g (Graph vs es) = F.foldMap (f . fst) es <> (F.foldMap g vs)
+
+-- identity + binary function (`mappend`)
+instance Monoid (Graph v e) where
+  mempty        = emptyGraph
+  g `mappend` h = Graph (g^.vertices ++ h^.vertices) (g^.edges ++ h^.edges)
