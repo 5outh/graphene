95d520a4684796ae06c92fd2cb0ef79118951826
Benjamin Kovach <bkovach13@gmail.com>
2014-04-23 21:14:59 -0400

    Add dijkstra comments

--- src/Graphene/Algorithms.hs
+++ src/Graphene/Algorithms.hs
@@ -49,53 +49,61 @@ dfs v g = case ns of
 bfs :: Eq v => v -> Graph e v -> [v]
 bfs v g = go [v] g
   where go []     _ = []
-        go (x:xs) g = x : go (xs ++ ns) (removeVertex x g)
+        go (x:xs) g = x : go (xs ++ ns) (removeVertex x g) -- add neighbors to queue
          where ns = neighbors x g
 
 -- test
 sg :: Graph Int Char
 sg = fromLists ['a'..'e'] (zip3 [1..5] ['a'..'d'] ['b'..'e'])
 
+infinity :: Int
+infinity = maxBound -- you get the idea
+
+-- Container for Dijkstra's algorithm information
 data DijkstraState e v = DijkstraState{
-    _underlyingGraph :: Graph e v
-  , _distancePairings :: M.Map v Int
-  , _prevs :: M.Map v (Maybe v)
-  , _unvisited :: [v]
-  , _visited :: [v]
-  , _from :: v
+    _underlyingGraph :: Graph e v     -- Graph to run algorithm on 
+  , _distancePairings :: M.Map v Int  -- Mapping from Vertices to Distances
+  , _prevs :: M.Map v (Maybe v)       -- Mapping from Vertices to previous vertices
+  , _unvisited :: [v]                 -- Set of unvisited vertices
+  , _visited :: [v]                   -- Set to visited vertices
+  , _from :: v                        -- Vertex to generate distances from
 } deriving (Show, Eq)
 
 makeLenses ''DijkstraState
 
 -- smart constructor for dijkstra state
+-- Initialize dist(v) to 0, the rest to inifinity
+-- Initialize previous vertices to nothing
 mkDijkstra :: (Eq v, Ord v) => Graph e v -> v -> DijkstraState e v
 mkDijkstra g@(Graph vs es) v = DijkstraState g dists prevs vs [] v
-  where dists = M.fromList ( (v, 0) : (map (, (maxBound :: Int) ) $ delete v vs) )
+  where dists = M.fromList ( (v, 0) : (map (, infinity) $ delete v vs) )
         prevs = M.fromList $ zip vs (repeat Nothing) 
 
+-- run dijkstra's algorithm on a graph starting at vertex v
 dijkstra :: (Eq v, Ord v) => Graph Int v -> v -> DijkstraState Int v
-dijkstra g = execState runDijkstra . mkDijkstra g
-
--- Graph, visited, unvisited
--- NB. ints are weights
-runDijkstra :: (Eq v, Ord v) => State (DijkstraState Int v) ()
-runDijkstra = do
-  q   <- use unvisited
-  unless (null q) $ do
-    dists <- use distancePairings
-    g     <- use underlyingGraph
-    let ordered@(u:_)  = sortBy (comparing (flip M.lookup dists)) q
-        (Just uWeight) = M.lookup u dists -- weight stored at `u`
-        conns          = map (\(e, (v1, v2)) -> (e, if v1 == u then v2 else v1)) 
-                       $ connections u g
-    unvisited %= (delete u) -- remove u from q
-    unless ( uWeight == (maxBound :: Int) ) $ do
-      -- update distances
-      forM_ conns $ \(eWeight, v) -> do
-        let (Just vWeight) = M.lookup v dists
-            newWeight      = eWeight + uWeight
-        unless (newWeight > vWeight) $ do
-          distancePairings %= (M.insert v newWeight)
-          prevs %= (M.insert v (Just u))
-      visited %= (u:)
-      runDijkstra
+dijkstra g = execState go . mkDijkstra g
+ where go :: (Eq v, Ord v) => State (DijkstraState Int v) ()
+       go = do
+         q   <- use unvisited
+         -- if unvisited set is empty, complete.
+         unless (null q) $ do 
+           dists <- use distancePairings
+           g     <- use underlyingGraph
+           let u  = minimumBy (comparing (flip M.lookup dists)) q -- find vertex with min. weight
+               (Just uWeight) = M.lookup u dists                  -- u's weight
+               -- list of edges (weights) and the vertices they point to
+               conns          = map (\(e, (v1, v2)) -> (e, if v1 == u then v2 else v1)) 
+                              $ connections u g
+           unvisited %= (delete u) -- remove u from q
+           -- if current weight is infinity, the graph is disconnected, so end.
+           unless ( uWeight == infinity ) $ do
+             -- update distances
+             forM_ conns $ \(eWeight, v) -> do
+               let (Just vWeight) = M.lookup v dists
+                   newWeight      = eWeight + uWeight
+               -- only update previous vertex and distance a smaller distance was found
+               unless (newWeight > vWeight) $ do
+                 distancePairings %= (M.insert v newWeight)
+                 prevs            %= (M.insert v (Just u) )
+             visited %= (u:) -- set u to visited
+             go              -- recursively run the algorithm
